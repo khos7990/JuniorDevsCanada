bd036ec37a0ddcbc190b0137f97b609e5b1e8e97
ishan <ishan.khosla22@gmail.com>
2022-09-20 20:42:36 -0400

    install and set up routes

---
+++ src/Components/Users.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export default function Users() {
+  const [users, setUsers] = useState();
+  return (
+    <div>
+      <h2>Users List</h2>
+    </div>
+  );
+}
