92d287ab756dc28a633accb689fb034ab419af64
ishan <ishan.khosla22@gmail.com>
2022-09-26 20:14:11 -0400

    create login function with jwt

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -22,6 +22,15 @@ async function create(req, res) {
   }
 }
 
-async function login(req, res) {}
+async function login(req, res) {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (req.body.password !== user.password) throw new Error();
+    const token = jwt.sign({ user }, process.env.SECRET, { expiresIn: "24h" });
+    res.status(200).json(token);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+}
 
 async function deleteUser(req, res) {}
