5257fb3622cb09cc72a8787eec6c64b894dc80ff
ishan <ishan.khosla22@gmail.com>
2022-09-27 20:33:11 -0400

    check if token has expired functionality

--- src/App.js
+++ src/App.js
@@ -15,6 +15,19 @@ export default class App extends Component {
     this.setState({ user: incomingUserData });
   };
 
+  componentDidMount() {
+    let token = localStorage.getItem("token");
+    if (token) {
+      const payload = JSON.parse(atob(token.split(".")[1]));
+      if (payload.exp <= Date.now() / 1000) {
+        localStorage.removeItem("token");
+        token = null;
+      } else {
+        this.setState({ user: payload.user });
+      }
+    }
+  }
+
   render() {
     return (
       <div className="App">
